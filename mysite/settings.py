7d32142b43229a27a62f91a89134e3e4ee7415de
Charlie Kelson <charlie.kelson@gettyimages.com>
2020-11-23 19:36:25 -0800

    Make project paths relative to os

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,9 @@ import os
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = '/Users/ckelson/uw_repos/python_230/lesson_6/django'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
